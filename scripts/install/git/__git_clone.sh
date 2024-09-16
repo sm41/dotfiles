c07cecc9e2f346b0da37f1dee56ae6a9464ae465
sm41 <simosimo.41@gmail.com>
2024-09-16 15:02:03 +0900

    fix script

--- scripts/install/git/__git_clone.sh
+++ scripts/install/git/__git_clone.sh
@@ -7,11 +7,16 @@ work_path=$(cd $(dirname $0) && pwd)
 # cd /opt
 
 
+
+
+
+
+
 while read repository_url
 do
 
   [ -z ${repository_url} ] && continue
-  [ ${repository_url::1} = "#" ] && continue
+  # [ ${repository_url::1} = "#" ] && continue
 
   echo "git clone ${repository_url}.git"
 
