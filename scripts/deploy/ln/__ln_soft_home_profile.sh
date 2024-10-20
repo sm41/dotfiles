958193ef32e90bb7bd0df81b570b511b421e447f
sm41 <39440128+sm41@users.noreply.github.com>
2024-10-20 16:50:25 +0900

    fix

--- scripts/deploy/ln/__ln_soft_home_profile.sh
+++ scripts/deploy/ln/__ln_soft_home_profile.sh
@@ -7,8 +7,12 @@ ROOT_DIR=/root
 FHS_DIR=/home
 
 # deploy .bashrc & .profile
-ln -s -b "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/sample.bash_profile"   "${HOME}/.bash_profile"
-ln -s -b "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/sample.bashrc"         "${HOME}/.bashrc"
-ln -s -b "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/sample.profile"        "${HOME}/.profile"
 
-source "${HOME}/.bash_profile"
+while read USER_PROFILE
+do
+  # echo "${USER_PROFILE}"
+  echo ""${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/${USER_PROFILE}"   "${HOME}/${USER_PROFILE#sample*}" "
+
+done < <( find "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}"  -maxdepth 1  -name "sample*"  -type f -printf '%f\n' | sort )
+
+# source "${HOME}/.bash_profile"
