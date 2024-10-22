f76bf5b685ac06fd353aca2e9dd1c97f4e69d59f
sm41 <39440128+sm41@users.noreply.github.com>
2024-10-22 13:54:42 +0900

    fix

--- scripts/deploy/ln/__ln_soft_home_profile.sh
+++ scripts/deploy/ln/__ln_soft_home_profile.sh
@@ -6,13 +6,20 @@ DOTFILES_DIR=/dotfiles
 ROOT_DIR=/root
 FHS_DIR=/home
 
-# deploy .bashrc & .profile
+aaa=".bash_profile"
+fff=".bashrc"
+rrr=".profile"
 
 while read USER_PROFILE
 do
-  # echo "${USER_PROFILE}"
-  echo ""${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/${USER_PROFILE}"   "${HOME}/${USER_PROFILE#sample*}" "
+  echo "ln -s -b   "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}/sample${USER_PROFILE}"   "${HOME}/${USER_PROFILE}" "
 
-done < <( find "${HOME}${REPOSITORY_DIR}${DOTFILES_DIR}${ROOT_DIR}${FHS_DIR}"  -maxdepth 1  -name "sample*"  -type f -printf '%f\n' | sort )
+done << edf
+  ${aaa}
+  ${fff}
+  ${rrr}
+edf
+
+exit 0
 
 # source "${HOME}/.bash_profile"
