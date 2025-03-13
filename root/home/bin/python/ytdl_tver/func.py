9167c6074edda40d1d1a671734e19e374a542b0f
sm41 <39440128+sm41@users.noreply.github.com>
2025-03-13 17:02:53 +0900

    fix

--- root/home/bin/python/ytdl_tver/func.py
+++ root/home/bin/python/ytdl_tver/func.py
@@ -1,8 +1,10 @@
 
+from subprocess import run
 from selenium import webdriver
+from pathlib  import Path
 from bs4  import BeautifulSoup
 from time import sleep
-from re   import compile
+from re  import compile
 from os  import getenv
 from sys import argv
 from mytool import abc
@@ -10,11 +12,12 @@ from mytool import abc
 
 class gen_var:
   def __init__(self):
-    self.arg = argv[1]
-    __env_dl    = getenv("CLIENT_NETWORK_STORAGE_misc")
-    __env_state = getenv("XDG_STATE_HOME")
+    self.arg          = argv[1]
+    __env_dl          = getenv("CLIENT_NETWORK_STORAGE_misc")
+    __env_state       = getenv("XDG_STATE_HOME")
     self.storage_path = abc.ctrl_path.anlys_path(__env_dl, "@tver")
     self.loaded_yaml  = abc.load_yaml(__env_state, "python", "tver.yaml")
+    self.y_dow        = abc.dow_yesterday(1)
 
 
 class scrp:
@@ -55,13 +58,63 @@ class rty:
   def find_key_value_list(self, data, target_key):
     if isinstance(data, dict):
       for key1, value1 in data.items():
-          if key1.startswith("_"):
-              continue
-          if "dow" not in value1:
-              # self.result_list.extend(self.find_key_value_list(value1, target_key))
-              self.find_key_value_list(value1, target_key)
-          else:
-              for dow_item in value1['dow']:
-                  if dow_item == target_key:
-                      self.result_list.append(value1)
-                      # break  # 同じtitleのデータが複数回追加されないようにする
+        if key1.startswith("_"):
+          continue
+        if "dow" not in value1:
+          self.find_key_value_list(value1, target_key)
+        else:
+          for dow_item in value1['dow']:
+            if dow_item == target_key:
+              self.result_list.append(value1)
+              # break  # 同じtitleのデータが複数回追加されないようにする
+
+
+class test:
+  def __init__(self):
+    self.__pre_series  = "series"
+    self.__pre_episode = "episode"
+    self.__pre_url     = "original_url"
+    self.__pre_filname = "filename"
+    self.__pre_ext     = "ext"
+    self.__pre_id      = "id"
+
+  def get_base_yaml(self, loaded_yaml):
+    self.config = loaded_yaml['tver']['_http']
+
+  def integrate(self, url, down_dir, yaml_config):
+    __header = yaml_config['header']
+    __cmd_ytdlp = [
+      "yt-dlp",
+        "--paths",  f"home:{down_dir}",
+        "--output", f"{__header}",
+        "--print",  self.__pre_series,
+        "--print",  self.__pre_episode,
+        "--print",  self.__pre_url,
+        "--print",  self.__pre_filname,
+        "--print",  self.__pre_ext,
+        "--print",  self.__pre_id,
+      url
+    ]
+    __ddd = run(__cmd_ytdlp, capture_output=True, text=True).stdout.strip()
+    self.series, self.episode, self.url, __ppp, self.ext, self.id = __ddd.splitlines()
+    self.paths, self.output = Path(__ppp).parent, abc.ctrl_file.zen2han(Path(__ppp).stem)
+
+
+def ytdlp(paths, id, ext, link):
+  method = [
+    "yt-dlp",
+      "--embed-subs",
+      "--paths",  str(paths),
+      "--output", f"{id}.{ext}",
+    link
+  ]
+  return method
+
+
+def ccc(series, episode, url, ext, id, paths, output):
+  method = ytdlp(paths, id, ext, url)
+  # print(method)
+  result = run(method)
+  output = abc.ctrl_file.byte_count(output, 245)
+  abc.ctrl_path.rnm(Path(paths, f"{id}.{ext}"), Path(paths, f"{output}.{ext}"))
+  abc.ntfy(result, f"{series}\n{episode}")
--- root/home/bin/python/ytdl_tver/main.py
+++ root/home/bin/python/ytdl_tver/main.py
@@ -1,35 +1,33 @@
 
 from sys import exit
-import func_dl, func
 from mytool import abc
+import func
 
 
 def main():
   abc.check_any.check_arg()
   variable = func.gen_var()
   scraper  = func.scrp()
-  testes   = func_dl.test()
+  testes   = func.test()
   dfgh     = func.rty()
 
   if variable.arg.startswith("https://tver.jp/episodes/"):
     testes.get_base_yaml(variable.loaded_yaml)
     testes.integrate(variable.arg, variable.storage_path, testes.config)
-    func_dl.ccc(testes.series, testes.episode, testes.url, testes.ext, testes.id, testes.paths, testes.output)
+    func.ccc(testes.series, testes.episode, testes.url, testes.ext, testes.id, testes.paths, testes.output)
 
   elif not variable.arg.startswith("https://tver.jp/episodes/"):
+
     if   variable.arg == "dow":
-      y_dow    = abc.dow_yesterday(1)
-      dfgh.find_key_value_list(variable.loaded_yaml, y_dow)
+      dfgh.find_key_value_list(variable.loaded_yaml, variable.y_dow)
     elif variable.arg != "dow":
       dfgh.find_key_dict(variable.loaded_yaml, variable.arg)
-
-    # print(dfgh.result_list)
 
     for bmw in dfgh.result_list:
       scraper.selenium(bmw["url"])
       scraper.tver(scraper.soup)
       testes.integrate(scraper.url, variable.storage_path, bmw)
-      func_dl.ccc(testes.series, testes.episode, testes.url, testes.ext, testes.id, testes.paths, testes.output)
+      func.ccc(testes.series, testes.episode, testes.url, testes.ext, testes.id, testes.paths, testes.output)
 
   else:
     exit("Invailed Argment!")
