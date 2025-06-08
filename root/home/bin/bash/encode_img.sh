7d9ff8526e68e07b85f36805bd7060675a02f46e
sm41 <39440128+sm41@users.noreply.github.com>
2025-06-08 22:14:35 +0900

    add

---
+++ root/home/bin/bash/encode_img.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -eu
+
+WORK_PATH="$(realpath $(dirname "$0"))"
+source "${WORK_PATH}/_func_rename_pipe.sh"
+
+old_ext=png
+new_ext=jpg
+arg_str="$1"
+base_path="$(dirname "${arg_str}")"
+old_dir="$(basename "${arg_str}")"
+new_dir="$(echo "${old_dir}_[encoded]" | zen2han)"
+
+function check_file(){
+  if find "${arg_str}" -type f -name "*.${old_ext}" | grep -q . ; then
+    :
+  else
+    echo "${FUNCNAME[0]}"
+    exit 1
+  fi
+}
+
+function check_argment(){
+  if [[ -d "${arg_str}" ]] ; then
+    check_file
+    old_path="${base_path}/${old_dir}"
+    new_path="${base_path}/${new_dir}"
+    mkdir -p "${new_path}"
+  elif [[ "${arg_str}" =~ ".${old_ext}"$ ]] ; then
+    old_path="${base_path}"
+    new_path="${base_path}"
+  else
+    echo "${FUNCNAME[0]}"
+    exit 1
+  fi
+}
+
+
+function main(){
+  while read inputfile; do
+
+    filename="${inputfile##*/}"
+    # old_ext="${filename##*.}"
+    old_file="${filename%.*}"
+    new_file="$(echo "${old_file}" | zen2han)"
+
+    ffmpeg -i "${old_path}/${old_file}.${old_ext}" \
+              "${new_path}/${new_file}.${new_ext}"
+
+    # echo "${new_path}/${new_file}.${new_ext}"
+
+  done < <(find "${arg_str}" -type f -name "*.${old_ext}" | sort -V)
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]] ; then
+  # check_file
+  check_argment
+  main
+fi
