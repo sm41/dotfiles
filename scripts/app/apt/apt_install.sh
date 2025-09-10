2dd6a746c1ecf34e033a63c7e794411bf3410bef
sm41 <39440128+sm41@users.noreply.github.com>
2025-09-11 05:01:57 +0900

    test

--- scripts/app/apt/apt_install.sh
+++ scripts/app/apt/apt_install.sh
@@ -17,14 +17,14 @@ work_path="$(realpath $(dirname "$0"))"
 input_file="$(find ${work_path}  -name ${type}   -not \( -path $0 \)  -type f  -printf "%f\n")"
 
 
-# sudo apt-get update
+sudo apt-get update
 
 while read package_name
 do
   [ -z "${package_name}" ] && continue
   [ "${package_name::1}" = "#" ] && continue
 
-  echo "${package_name}"
-  # sudo apt-get install --no-install-recommends ^"${package_name}"$
+  # echo "${package_name}"
+  sudo apt-get install --no-install-recommends ^"${package_name}"$
 
 done < "${work_path}/${input_file}"
