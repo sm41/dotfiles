204f9e7554b03994f0523fbb9ed76abe27ee1d55
sm41 <39440128+sm41@users.noreply.github.com>
2025-12-25 01:43:21 +0900

    fix

--- root/home/bin/python/scraping/analyse.py
+++ root/home/bin/python/scraping/analyse.py
@@ -14,18 +14,18 @@ class website:
     self.page    = self.scrp.soup.find(class_ = compile("el_articleCard")).attrs['href']
 
 
-  def dariaartificial(self):
-    self.website = "DariaArtificial"
-    self.url     = "https://twstalker.com/DariaArtificial"
-    self.soup    = self.scrp.get_response(self.url).make2soup().simple("html.parser").soup
-    self.article = self.website
-    self.page    = "https://twstalker.com" + self.scrp.soup.select_one("div > span > a").attrs['href']
-    # https://x.com/i/user/1690092058055233536
+  # def dariaartificial(self):
+  #   self.website = "DariaArtificial"
+  #   self.url     = "https://twstalker.com/DariaArtificial"
+  #   self.soup    = self.scrp.get_response(self.url).make2soup().simple("html.parser").soup
+  #   self.article = self.website
+  #   self.page    = "https://twstalker.com" + self.scrp.soup.select_one("div > span > a").attrs['href']
+  #   # https://x.com/i/user/1690092058055233536
 
 
-  def mnemosynekoto(self):
-    self.website = "mnemosynekoto"
-    self.url     = "https://kemono.cr/patreon/user/86508534"
-    self.soup    = self.scrp.get_response(self.url).make2soup().simple("html.parser").soup
-    self.article = self.website
-    self.page    = "https://kemono.cr" + self.scrp.soup.select_one("div > article > a").attrs['href']
+  # def mnemosynekoto(self):
+  #   self.website = "mnemosynekoto"
+  #   self.url     = "https://kemono.cr/patreon/user/86508534"
+  #   self.soup    = self.scrp.get_response(self.url).make2soup().simple("html.parser").soup
+  #   self.article = self.website
+  #   self.page    = "https://kemono.cr" + self.scrp.soup.select_one("div > article > a").attrs['href']
