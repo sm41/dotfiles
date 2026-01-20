e06121ed95a4b8c1c8864f8758acedd0972e655c
sm41 <39440128+sm41@users.noreply.github.com>
2026-01-20 21:52:52 +0900

    fix

--- root/home/bin/python/mytool/notify.py
+++
@@ -1,7 +0,0 @@
-from plyer import notification
-
-def ntfy(result, text):
-  if result.returncode == 0:
-    notification.notify(title = "✅ Success", message = text)
-  else:
-    notification.notify(title = "⚠️ failed", message = text)
