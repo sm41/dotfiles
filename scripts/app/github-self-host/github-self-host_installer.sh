3d9d82a4c3a67332fbdad24c63158de642e2c4fd
sm41 <39440128+sm41@users.noreply.github.com>
2026-05-29 00:06:37 +0900

    fix

--- scripts/app/github-self-host/github-self-host_installer.sh
+++ scripts/app/github-self-host/github-self-host_installer.sh
@@ -6,9 +6,6 @@ container_repo_list=(
     # Mirakurun EPGStation
     "https://github.com/l3tnun/docker-mirakurun-epgstation"
 
-    # # qbittorrent-nox
-    # https://github.com/qbittorrent/docker-qbittorrent-nox
-
     # tt-rss
     "https://github.com/tt-rss/tt-rss"
 
