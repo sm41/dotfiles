2b25db5a1b450bcb691c687b6440ec83d09d0fb4
sm41 <39440128+sm41@users.noreply.github.com>
2026-07-18 13:38:57 +0900

    fix

--- scripts/openwrt/force-workflow-dispatch/force-workflow-dispatch.sh
+++ scripts/openwrt/force-workflow-dispatch/force-workflow-dispatch.sh
@@ -1,37 +1,22 @@
 #!/bin/sh
 set -eu
 
-. .env
+rrr=$(cd $(dirname $0) && pwd)
+. ${rrr}/.env
 
-
-# curl \
-#     -H "Accept: application/vnd.github.v3+json" \
-#     -H "Authorization: token xxxxxxxxxxxx" \
+# wget \
+#     --header="Accept: application/vnd.github+json" \
+#     --header="Authorization: Bearer ${token}" \
 #     "https://api.github.com/repos/sm41/cicd-feed/actions/workflows"
 
-ccc(){
-    curl \
-        -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${token}" \
-    "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow_id}/dispatches" \
-        -d '{
-            "ref":"main"
-        }'
-}
-
-www(){
-    wget \
-        --method=POST \
-        --header="Accept: application/vnd.github+json" \
-        --header="Authorization: Bearer ${token}" \
-        --header="Content-Type: application/json" \
-        --body-data='{
-            "ref":"main"
-        }' \
-        -O - \
-    "https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow_id}/dispatches"
-}
-
-www
+wget \
+    --method=POST \
+    --header="Accept: application/vnd.github+json" \
+    --header="Authorization: Bearer ${token}" \
+    --header="Content-Type: application/json" \
+    --body-data='{
+        "ref":"main"
+    }' \
+    -O - \
+"https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflow_id}/dispatches"
 
